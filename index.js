b960064c4c93f85446411985c29a62683497de0b
feisalbireh <feisal.bireh@student.moringaschool.com>
2022-11-25 12:20:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (blk="*"){
+    return function(adjective="a hard worker"){
+        return `You are ${blk}${adjective}${blk}!`
+    }
+     
+}
+
